c0c68bdc194cd96ff6dda0570c06b9d6435e5d0c
VictorChang76 <43318235+VictorChang76@users.noreply.github.com>
2019-07-13 10:03:15 +0800

    Calculating data automatically. Use scatter() to plot

--- maplotlib_1st_try.py
+++ maplotlib_1st_try.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-	input_values = [1, 2, 3, 4, 5, 6, 7]
-	squares = [1, 4, 9, 16, 25, 36, 49]
+	x_values = range(1, 1001)
+	y_values = [x**2 for x in x_values]
 
+	plt.style.use('seaborn')
 	fig, ax = plt.subplots()
-	ax.plot(squares, linewidth = 3)
+	# ax.plot(input_values, squares, linewidth = 3)
+	ax.scatter(x_values, y_values, s=1)
 
 	# Set chart title and label axes.
 	ax.set_title("Square Numbers", fontsize=24)
@@ -15,5 +17,7 @@ if __name__ == "__main__":
 	# Set size of tick labels.
 	ax.tick_params(axis="both", labelsize=14)
 
+	# Set the range of each axis.
+	ax.axis([1, 1100, 1, 1_100_000])
 
 	plt.show()
